79b7e4c21857ebcb8c7a1a6d3134f95f103292a2
Wally Bana <mahamanebana@gmail.com>
2023-01-21 15:25:48 -0500

    put some commemnts

--- calculator.c
+++ calculator.c
@@ -1,42 +1,32 @@
 #include<stdio.h>
 #include<stdlib.h>
-
+/*add */
 int add(int x, int y){
-
 	return x + y ; 
-
 }
-
+/*Substraction*/
 int minus(int x, int y){
 	return x - y ; 
 }
 
+/*Division*/
 int  divise (int x, int y){
-
 	if(y == 0){
 		printf("\nCannot divide a number by zero\n");
-		
 	}
 	else
 	return x/y;
-
-
 }
 
+/*Multiplication*/
 int mult(int x, int y){
-
-
 	return x * y ; 
-
 }
 
 
 int main(){
-
-
 	int x = 10; 
 	int y = 0; 
-
 	printf("\n%d + %d = %d\n", x, y, add(x,y)); 
 	printf("\n%d - %d = %d\n", x, y, minus(x,y)); 
 	printf("%\n%d / %d = %d \n", x, y, divise(x, y)); 
